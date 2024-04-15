3fcc324af004f91492a1b6593e285e86aa1821a6
Oleh Morozov <olegmorozov140988@gmail.com>
2024-04-15 17:19:33 +0200

    CreateAccountFromDataProviderWithCsvFileTests_Fixsed

--- src/test/java/com/ait/qa34/homeWorks/tests/TestBase.java
+++ src/test/java/com/ait/qa34/homeWorks/tests/TestBase.java
@@ -1,16 +1,21 @@
 package com.ait.qa34.homeWorks.tests;
 
 import com.demowebshop.fw.ApplicationManager;
+import com.demowebshop.models.User;
 import org.openqa.selenium.remote.Browser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class BasePage {
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
@@ -43,5 +48,6 @@ public class BasePage {
         logger.info("Stop test");
         logger.info("======================================================================");
     }
+
 }
 
